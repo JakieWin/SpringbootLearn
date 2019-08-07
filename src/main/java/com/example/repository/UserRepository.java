218644ca8222ac7a5a47a3784a069f68e60df9d1
jk <2843188370@qq.com>
2019-08-07 11:21:55 +0800

    delete

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -3,7 +3,10 @@ package com.example.repository;
 import com.example.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User,Long> {
     User findByUserName(String userName);
     User findByUserNameOrEmail(String userName,String email);
+    List<User> findByUserNameContaining(String userName);
 }
